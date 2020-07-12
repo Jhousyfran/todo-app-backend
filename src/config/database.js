90fbd5c072c0fd2bf4a6bbffebc6cbe5891e56ff
Jhousyfran Costa <jhousyfrancosta@gmail.com>
2020-07-12 01:06:43 -0300

    improvements for warning removal

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ mongoose.Promise = global.Promise
 
 const { DB_HOST, DB_PORT, DB_NAME } = process.env
 module.exports = mongoose.connect(
-    `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`)
+    `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`, { useNewUrlParser: true, useUnifiedTopology: true })
 
 mongoose.connection.on('error', () => console.error('connection error:'))
 mongoose.connection.once('open', () => console.log('database connected'))
